3b80ff0af47812b00a19a3d978c40d5e5a05dcbc
Maid <zhp92@outlook.com>
2022-07-07 17:53:18 +0800

    feat: :construction: Put conf into global

--- common/global/global.go
+++ common/global/global.go
@@ -1,11 +1,9 @@
 package global
 
 import (
-	"fmt"
+	"memos/common/global/config"
 
-	"github.com/fsnotify/fsnotify"
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
 const (
@@ -14,40 +12,15 @@ const (
 
 var (
 	Engine *gin.Engine
-	Viper  *viper.Viper
+	Config *config.Config
 )
 
-func GetEngine() *gin.Engine {
-	if Engine == nil {
-		Engine = gin.New()
-	}
-	return Engine
+func GetConfig() *config.Config {
+	Config = config.Conf
+	return Config
 }
 
-func GetViper() *viper.Viper {
-	if Viper == nil {
-		Viper = viper.New()
-		initViper()
-	}
-	return Viper
-}
-
-func initViper() {
-	// set default config file
-	Viper.SetConfigName("settings")
-	Viper.SetConfigType("yaml")
-	Viper.AddConfigPath("./")
-	Viper.AddConfigPath("./config/")
-	Viper.AddConfigPath(".")
-	err := Viper.ReadInConfig()
-	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-		fmt.Println("config file not found")
-		panic(fmt.Errorf("fatal error config file:%s", err))
-	}
-	// watch conf file changed
-	viper.WatchConfig()
-	viper.OnConfigChange(func(e fsnotify.Event) {
-		// callback if config file changed
-		fmt.Println("config file changed:", e.Name)
-	})
+func init() {
+	Engine = gin.New()
+	Config = config.Conf
 }
