5451a8d344619d237e425e82c0132150105c4466
Maid <zhp92@outlook.com>
2022-07-24 12:52:50 +0800

    refactor: :recycle: auth modules

--- server/auth/Role.go
+++ server/auth/Role.go
@@ -5,10 +5,10 @@ type Role struct {
 	Authorities []Authority `json:"authority"`
 }
 
-var adminAuthorities = []Authority{AUTHORITY_BROWSE, AUTHORITY_EDIT, AUTHORITY_LOGIN}
-var visitorAuthorities = []Authority{AUTHORITY_BROWSE, AUTHORITY_EDIT, AUTHORITY_LOGIN}
+var adminAuthorities = []Authority{AUTHORITY_BROWSE, AUTHORITY_EDIT}
+var visitorAuthorities = []Authority{AUTHORITY_BROWSE}
 
 var (
-	ROLE_ADMIN   = Role{"admin", adminAuthorities}
-	ROLE_VISITOR = Role{"visitor", visitorAuthorities}
+	ROLE_ADMIN   = &Role{"admin", adminAuthorities}
+	ROLE_VISITOR = &Role{"visitor", visitorAuthorities}
 )
