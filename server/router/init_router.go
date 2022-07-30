d0b83050de87f5b79cc4cc25cacd71ec06a280ab
Maid <zhp92@outlook.com>
2022-07-30 16:53:46 +0800

    refactor: :construction: (wip) refactor router module

---
+++ server/router/init_router.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+  "memos/server/config"
+  "memos/server/logger"
+  "strconv"
+
+  "github.com/gin-gonic/gin"
+)
+
+var Routers = make([]func(*gin.RouterGroup), 0)
+var R *gin.Engine
+
+func InitRouter() {
+  R = gin.Default()
+  v1 := R.Group("/api/v1")
+  for _, f := range Routers {
+    f(v1)
+  }
+}
+
+func RunServer() {
+  addr := config.Conf.Application.Host + ":" + strconv.Itoa(config.Conf.Application.Port)
+  R.Use(logger.GinLogger(), logger.GinRecovery(true))
+  if err := R.Run(addr); err != nil {
+    panic(err)
+  }
+}
