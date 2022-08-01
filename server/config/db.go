d36b22b443ab528f1f7c2ff5ed80689959760a24
Maid <zhp92@outlook.com>
2022-08-01 15:46:11 +0800

    refactor: :recycle: remove the Driver  property of Db

--- server/config/db.go
+++ server/config/db.go
@@ -1,12 +1,11 @@
 package config
 
 type Db struct {
-	Driver   DbDirver `mapstructure:"driver"`
-	Host     string   `mapstructure:"host"`
-	Port     int      `mapstructure:"port"`
-	Name     string   `mapstructure:"name"`
-	User     string   `mapstructure:"user"`
-	Password string   `mapstructure:"password"`
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Name     string `mapstructure:"name"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
 }
 
 type DbDirver string
