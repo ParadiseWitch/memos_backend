1f7d44ac2f76ffd995210b5f18d572ceb6932634
Maid <zhp92@outlook.com>
2022-09-28 17:19:18 +0800

    refactor: refactor service

--- server/service/sys_service.go
+++ server/service/sys_service.go
@@ -1,22 +1,48 @@
 package service
 
 import (
+	"memos/server/db"
 	"memos/server/dto"
+	"memos/server/logger"
+	"memos/server/util"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+const TABLENAME = "sys_conf"
+
 func GetSysConfById(c *gin.Context) {
 	var s dto.SysConf
-	GetById("sys_conf", s, c)
+	id, ok := c.GetQuery("id")
+	if !ok {
+		logger.Errorf("GetUserById err, id: %s", id)
+		c.JSON(http.StatusBadRequest,
+			util.CommonFailRes(http.StatusBadRequest, "id is required"))
+	}
+	db.DB.Table(TABLENAME).Where("id = ?", id).Find(&s)
+	c.JSON(http.StatusOK, util.CommonSuccRes(s))
 }
 
 func UpdateSysConfById(c *gin.Context) {
 	var s dto.SysConf
-	UpdateById("sys_conf", s, c)
+	err := c.BindJSON(&s)
+	if err != nil {
+		logger.Errorf("UpdateUserById err, err: %s", err)
+		c.JSON(http.StatusBadRequest,
+			util.CommonFailRes(http.StatusBadRequest, "invalid request"))
+	}
+	db.DB.Table(TABLENAME).Save(&s)
 }
 
 func AddSysConf(c *gin.Context) {
 	var s dto.SysConf
-	Add("sys_conf", s, c)
+	err := c.BindJSON(&s)
+	if err != nil {
+		logger.Errorf("AddUser err, err: %s", err)
+		c.JSON(http.StatusBadRequest,
+			util.CommonFailRes(http.StatusBadRequest, "invalid request"))
+	}
+	db.DB.Table(TABLENAME).Create(&s)
+	c.JSON(http.StatusOK, util.CommonSuccRes(s))
 }
--- server/service/user_service.go
+++ server/service/user_service.go
@@ -3,31 +3,44 @@ package service
 import (
 	"memos/server/db"
 	"memos/server/dto"
+	"memos/server/logger"
+	"memos/server/util"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 )
-
-var userTable *gorm.DB
-
-func getUserTable() *gorm.DB {
-	if userTable == nil {
-		userTable = db.DB.Table("user")
-	}
-	return userTable
-}
 
 func GetUserById(c *gin.Context) {
 	var u dto.User
-	GetById("user", u, c)
+	id, ok := c.GetQuery("id")
+	if !ok {
+		logger.Errorf("GetUserById err, id: %s", id)
+		c.JSON(http.StatusBadRequest,
+			util.CommonFailRes(http.StatusBadRequest, "id is required"))
+	}
+	db.DB.Table(TABLENAME).Where("id = ?", id).Find(&u)
+	c.JSON(http.StatusOK, util.CommonSuccRes(u))
 }
 
 func UpdateUserById(c *gin.Context) {
 	var u dto.User
-	UpdateById("user", u, c)
+	err := c.BindJSON(&u)
+	if err != nil {
+		logger.Errorf("UpdateUserById err, err: %s", err)
+		c.JSON(http.StatusBadRequest,
+			util.CommonFailRes(http.StatusBadRequest, "invalid request"))
+	}
+	db.DB.Table(TABLENAME).Save(&u)
 }
 
 func AddUser(c *gin.Context) {
 	var u dto.User
-	Add("user", u, c)
+	err := c.BindJSON(&u)
+	if err != nil {
+		logger.Errorf("AddUser err, err: %s", err)
+		c.JSON(http.StatusBadRequest,
+			util.CommonFailRes(http.StatusBadRequest, "invalid request"))
+	}
+	db.DB.Table(TABLENAME).Create(&u)
+	c.JSON(http.StatusOK, util.CommonSuccRes(u))
 }
