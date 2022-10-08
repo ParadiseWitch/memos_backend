89a992ed60fddac921dee73a89f03c0475f01d32
Maid <zhp92@outlook.com>
2022-10-08 21:38:45 +0800

    feat: jwt

---
+++ server/pkg/jwt/jwt.go
@@ -0,0 +1,46 @@
+package jwt
+
+import (
+	"errors"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type MyClaims struct {
+	UserID int `json:"user_id"`
+	jwt.StandardClaims
+}
+
+var mySecret = []byte("memos")
+
+const TokenExpireDuration = time.Hour * 24 * 365
+
+func keyFunc(_ *jwt.Token) (i interface{}, err error) {
+	return mySecret, nil
+}
+
+func GetToken(userID int) (token string, err error) {
+	c := MyClaims{
+		userID, // 自定义字段
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(TokenExpireDuration).Unix(),
+			Issuer:    "memos",
+		},
+	}
+	token, err = jwt.NewWithClaims(jwt.SigningMethodHS256, c).SignedString(mySecret)
+	return
+}
+
+func ParseToken(tokenString string) (claims *MyClaims, err error) {
+	var token *jwt.Token
+	claims = new(MyClaims)
+	token, err = jwt.ParseWithClaims(tokenString, claims, keyFunc)
+	if err != nil {
+		return
+	}
+	if !token.Valid {
+		err = errors.New("invalid token")
+	}
+	return
+}
